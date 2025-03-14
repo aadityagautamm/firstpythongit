2c1d847da8e1ee5fd96a2084efc0be049a1b1133
aadityagautamm <gautamaadityaa1031@gmail.com>
2025-03-14 20:54:03 +0545

    nextcommit

---
+++ mainpractice.py
@@ -0,0 +1,38 @@
+# #functions with nested function
+
+# def talk(phrase):
+#     def say(word):
+#         print(word)
+#     words = phrase.split(' ')
+#     for word in words:
+#         say(word)
+
+# talk('hello world')
+ 
+def time_input ():
+    while True:
+        try:
+            hours = int(input("Enter hours: "))
+            minutes = int(input("Enter minutes: "))
+            seconds = int(input("Enter seconds: "))
+            
+            if 0 <= hours <= 23 and 0 <= minutes <= 59 and 0 <= seconds <= 59:
+                return f"{hours:02}:{minutes:02}:{seconds:02}"  
+            else:
+                    print("Invalid input. Please enter hours between 0 and 23, minutes between 0 and 59, and seconds between 0 and 59.")
+        
+        except ValueError:
+          
+            print("Invalid input. Please enter numeric values.")
+          
+timestamp = time_input()
+print (timestamp)
+
+if timestamp > '16:00:00':
+    print ("Good Evening")
+elif timestamp > '12:00:00':
+    print ("Good Afternoon")
+else:
+    print ("Good Morning")
+
+
